597f898ecf38aadd9df65a4b6a4eaec2f207812e
monika <moni1234566@gmail.com>
2020-03-10 20:36:28 +0100

    Started working on create character ui.

---
+++ Assets/Scripts/Manage/UI/CreateCharacter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using Manage.Characters;
+using Manage.Units;
+
+namespace Manage.UI
+{
+    public class CreateCharacter : MonoBehaviour
+    {
+        public InputField FirstNameInputField;
+        public InputField SurnameInputField;
+        public InputField NicknameInputField;
+        public InputField AgeInputField;
+        public Button RandomizeFirstNameButton;
+        public Button RandomizeSurnameButton;
+        public Button RandomizeNicknameButton;
+        public Button RandomizeAgeButton;
+        public Button FemaleButton;
+        public Button MaleButton;
+        public Button OtherButton;
+    }
+}
