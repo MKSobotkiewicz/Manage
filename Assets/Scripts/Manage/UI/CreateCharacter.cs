5bac3e907644b69c238f15fc96fd3ee78bf9729c
monika <moni1234566@gmail.com>
2020-03-11 06:41:49 +0100

    Work on CreateCharacter editor.

--- Assets/Scripts/Manage/UI/CreateCharacter.cs
+++ Assets/Scripts/Manage/UI/CreateCharacter.cs
@@ -23,5 +23,57 @@ namespace Manage.UI
         public Button FemaleButton;
         public Button MaleButton;
         public Button OtherButton;
+        private Texture2D Portrait;
+
+        public CharacterCultureTypes.ECharacterCulture CharacterCulture;
+
+        private EGender gender = EGender.Female;
+
+        public void Start()
+        {
+            RandomizeFirstName();
+            RandomizeSurname();
+            RandomizeNickname();
+            RandomizeAge();
+            RandomizePortrait();
+        }
+
+        public void SetGenderFemale()
+        {
+            gender = EGender.Female;
+        }
+        public void SetGenderMale()
+        {
+            gender = EGender.Male;
+        }
+        public void SetGenderOther()
+        {
+            gender = EGender.Other;
+        }
+
+        public void RandomizeFirstName()
+        {
+            FirstNameInputField.text = CharacterCultureTypes.ToCharacterCulture(CharacterCulture).RandomFirstName(gender);
+        }
+
+        public void RandomizeSurname()
+        {
+            SurnameInputField.text = CharacterCultureTypes.ToCharacterCulture(CharacterCulture).RandomSurname();
+        }
+
+        public void RandomizeNickname()
+        {
+            NicknameInputField.text = CharacterCultureTypes.ToCharacterCulture(CharacterCulture).RandomNickname(gender);
+        }
+
+        public void RandomizeAge()
+        {
+            AgeInputField.text = CharacterCultureTypes.ToCharacterCulture(CharacterCulture).RandomNickname(gender);
+        }
+
+        public void RandomizePortrait()
+        {
+            Portrait = CharacterCultureTypes.ToCharacterCulture(CharacterCulture).RandomIcon(gender);
+        }
     }
 }
