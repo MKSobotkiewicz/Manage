646041f7cd3f31f12f941f462327a06b8050fd29
monika <moni1234566@gmail.com>
2020-03-26 10:44:57 +0100

    Settings and audio development.

---
+++ Assets/Code/Manage/Properties/Audio.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace Manage.Settings
+{
+    public class Audio:MonoBehaviour
+    {
+        public AudioMixer MasterAudioMixer;
+
+        public float MasterVolume { get; private set; }
+        public float SoundEffectsVolume { get; private set; }
+        public float MusicVolume { get; private set; }
+
+        void OnLevelWasLoaded(int level)
+        {
+            MasterVolume = PlayerPrefs.GetFloat("MasterVolume");
+            SoundEffectsVolume = PlayerPrefs.GetFloat("SoundEffectsVolume");
+            MusicVolume = PlayerPrefs.GetFloat("MusicVolume");
+
+            MasterAudioMixer.SetFloat("Master", MasterVolume);
+            MasterAudioMixer.SetFloat("GameEffects", SoundEffectsVolume);
+            MasterAudioMixer.SetFloat("Music", MusicVolume);
+        }
+
+        public void SetMasterVolume(float masterVolume)
+        {
+            MasterVolume = masterVolume;
+            PlayerPrefs.SetFloat("MasterVolume", MasterVolume);
+            PlayerPrefs.Save();
+            MasterAudioMixer.SetFloat("Master", MasterVolume);
+        }
+
+        public void SetSoundEffectsVolume(float soundEffectsVolume)
+        {
+            SoundEffectsVolume = soundEffectsVolume;
+            PlayerPrefs.SetFloat("SoundEffectsVolume", SoundEffectsVolume);
+            PlayerPrefs.Save();
+            MasterAudioMixer.SetFloat("GameEffects", SoundEffectsVolume);
+        }
+
+        public void SetMusicVolume(float musicVolume)
+        {
+            MusicVolume = musicVolume;
+            PlayerPrefs.SetFloat("MusicVolume", MusicVolume);
+            PlayerPrefs.Save();
+            MasterAudioMixer.SetFloat("Music", MusicVolume);
+        }
+    }
+}
---
+++ Assets/Code/Manage/UI/AudioSettings.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+namespace Manage.UI
+{
+    public class AudioSettings : MonoBehaviour
+    {
+        public Settings.Audio Audio;
+        public Slider MasterVolumeSlider;
+        public Slider MusicVolumeSlider;
+        public Slider EffectsVolumeSlider;
+
+        public void Start()
+        {
+            MasterVolumeSlider.value = Audio.MasterVolume;
+            MusicVolumeSlider.value = Audio.MusicVolume;
+            EffectsVolumeSlider.value = Audio.SoundEffectsVolume;
+
+            MasterVolumeSlider.onValueChanged.AddListener(delegate { SetMasterVolume(); });
+            MusicVolumeSlider.onValueChanged.AddListener(delegate { SetMusicVolume(); });
+            EffectsVolumeSlider.onValueChanged.AddListener(delegate { SetEffectsVolume(); });
+        }
+
+        public void SetMasterVolume()
+        {
+            Audio.SetMasterVolume(MasterVolumeSlider.value);
+        }
+
+        public void SetMusicVolume()
+        {
+            Audio.SetMusicVolume(MusicVolumeSlider.value);
+        }
+
+        public void SetEffectsVolume()
+        {
+            Audio.SetSoundEffectsVolume(EffectsVolumeSlider.value);
+        }
+    }
+}
