9302bc2f5b469babfdb5d289c941bff52bdd3549
monika <moni1234566@gmail.com>
2020-05-02 19:15:07 +0200

    Added blinking sign.

---
+++ Assets/Code/Manage/Utilities/Sign.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Manage.Utilities
+{
+    public class Sign:MonoBehaviour
+    {
+        public MeshRenderer MeshRenderer;
+        public Light Light;
+
+        private float timer=0;
+        private bool isEmissive=false;
+
+        private static readonly System.Random random = new System.Random();
+
+        public void Update()
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                timer = (float)(random.NextDouble()+1);
+                if (isEmissive)
+                {
+                    MeshRenderer.material.SetFloat("_isEmissive",0);
+                    Light.gameObject.SetActive(false);
+                    isEmissive = false;
+                }
+                else
+                {
+                    MeshRenderer.material.SetFloat("_isEmissive", 1);
+                    Light.gameObject.SetActive(true);
+                    isEmissive = true;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Resources/Buildings/Signs/SignShader.shader
@@ -0,0 +1,43 @@
+﻿Shader "Custom/SignShader"
+{
+    Properties
+    {
+        [HDR]_Color ("Color", Color) = (1,1,1,1)
+        _MainTex ("Albedo (RGB)", 2D) = "white" {}
+		_MetallicGlossMap("Metallic", 2D) = "black" {}
+		[MaterialToggle] _isEmissive("Emmision", Float) = 0
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 200
+
+        CGPROGRAM
+        #pragma surface surf Standard fullforwardshadows
+
+		#pragma target 5.0
+
+		sampler2D _MainTex;
+		sampler2D _MetallicGlossMap;
+
+        struct Input
+        {
+            float2 uv_MainTex;
+        };
+
+        fixed4 _Color;
+		float _isEmissive;
+
+        void surf (Input IN, inout SurfaceOutputStandard o)
+        {
+			fixed4 c = tex2D(_MainTex, IN.uv_MainTex);
+			fixed4 m = tex2D(_MetallicGlossMap, IN.uv_MainTex);
+			o.Albedo = c.rgb;
+			o.Metallic = m.rgb;
+			o.Smoothness = m.a;
+			o.Emission = _isEmissive * _Color;
+        }
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
