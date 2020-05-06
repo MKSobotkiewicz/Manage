ff8e42b0542cd98e89e27f641d556aa553b9c520
monika <moni1234566@gmail.com>
2020-05-06 20:23:12 +0200

    Misc and ambient.

---
+++ Assets/Code/Manage/Audio/MusicManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Manage.Audio
+{
+    public class MusicManager : MonoBehaviour
+    {
+        public List<AudioClip> Tracks;
+        public bool PlayOnAwake = false;
+
+        private AudioSource audioSource;
+
+        protected static System.Random random = new System.Random();
+
+        void Awake()
+        {
+            Tracks = new List<AudioClip>();
+            foreach (var track in UnityEngine.Resources.LoadAll("Audio/MusicTracks", typeof (AudioClip)))
+            {
+                Tracks.Add(track as AudioClip);
+            }
+            audioSource = GetComponent<AudioSource>();
+            if (PlayOnAwake)
+            {
+                Play();
+            }
+        }
+
+        void Start()
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+        void Update()
+        {
+            if (!audioSource.isPlaying)
+            {
+                Play();
+            }
+        }
+
+        public void Play()
+        {
+            if (Tracks.Count <= 0)
+            {
+                return;
+            }
+            audioSource.PlayOneShot(Tracks[random.Next(0, Tracks.Count - 1)]);
+        }
+    }
+}
